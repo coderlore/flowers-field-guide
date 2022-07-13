de47e8aa7a5f96366c6df0b736696dd830d4fb5b
coderlore <coderlore@gmail.com>
2022-07-12 21:03:43 -1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient =require('mongodb').MongoClient
+require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
