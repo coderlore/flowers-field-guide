30ffa8618e5353b3797a003faf1b185d057d4465
coderlore <coderlore@gmail.com>
2022-09-25 22:08:40 -1000

    Troubleshooting mongodb error

--- public/main.js
+++ public/main.js
@@ -1,14 +1,13 @@
 let searchBtn = document.querySelector("#search")
-searchBtn.addEventListener('click', apiRequest)
+// searchBtn.addEventListener('click', apiRequest)
 const plantName = document.querySelector('input').value
 console.log(plantName)
 
 async function apiRequest() {
-    const plantName = document.querySelector('input').value
+    const plantName = document.querySelector(searchBtn).value
     try{
         const response = await fetch(`https://flower-field-guide-api.herokuapp.com/api/${plantName}`)
         const data = await response.json()
-        console.log(data)
 
         document.getElementById('plantName').innerText = data.name
         document.getElementById('pronounce').innerText = data.pronounce
