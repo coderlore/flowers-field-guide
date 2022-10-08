0f1cb2a044a5819f6ee1ccebab6db666fa25eb5c
coderlore <coderlore@gmail.com>
2022-10-08 13:56:17 -1000

    Fixed signit and login button styling, fixed api search

--- public/main.js
+++ public/main.js
@@ -1,10 +1,11 @@
 let searchBtn = document.querySelector("#search")
-// searchBtn.addEventListener('click', apiRequest)
+searchBtn.addEventListener('click', apiRequest)
 const plantName = document.querySelector('input').value
-console.log(plantName)
+// console.log(plantName)
 
 async function apiRequest() {
-    const plantName = document.querySelector(searchBtn).value
+    const plantName = document.querySelector("input").value
+
     try{
         const response = await fetch(`https://flower-field-guide-api.herokuapp.com/api/${plantName}`)
         const data = await response.json()
